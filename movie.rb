a9b0ea91a6fa0a35e57b1542a801341c75654ecc
Simon Coffey <simon@urbanautomaton.com>
2019-07-16 07:45:02 +0100

    Directly output twitter-valid movie, disable looper
    The looper doesn't work from the script because backticks execute with
    'sh -c "your-command"', and the command's got bash process substitution
    in it. Too lazy to fix it rn.

--- movie.rb
+++ movie.rb
@@ -3,6 +3,5 @@
   `./target/release/raygrass --samples 50 --resolution 1280x960 --time #{time} frames/#{"%03d" % frame}.png`
 end
 
-`ffmpeg -r 25 -pattern_type glob -i 'frames/*.png' -c:v libx264 out.mp4`
-`ffmpeg -f concat -safe 0 -i <(for i in {1..4}; do printf "file '%s'\n" "${PWD}/out.mp4"; done) -c copy looped.mp4`
-`ffmpeg -i looped.mp4 -vcodec libx264 -pix_fmt yuv420p -strict -2 -acodec aac twitter.mp4`
+`ffmpeg -r 25 -pattern_type glob -i 'frames/*.png' -c:v libx264 -pix_fmt yuv420p -strict -2 -acodec aac out.mp4`
+# `ffmpeg -f concat -safe 0 -i <(for i in {1..4}; do printf "file '%s'\n" "${PWD}/out.mp4"; done) -c copy looped.mp4`
