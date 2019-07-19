7ffff2a74e9538f3c5312f0ba31807cc09db2a7a
Simon Coffey <simon@urbanautomaton.com>
2019-07-19 22:22:08 +0100

    Use sines for checkerboard texture
    I switched to cosines cos of a weird boundary condition for the plane at
    y = 0, but now I'm using uv coordinates we can switch to sines - this
    means you start at a 0 so you get a full phase before the first switch,
    instead of half a phase.
    
    You can see the effect at the top of the sphere where previously you got
    a small longitudinal segment to start with.

--- src/texture.rs
+++ src/texture.rs
@@ -26,9 +26,9 @@ pub struct CheckerboardTexture<T1: Texture, T2: Texture> {
 impl<T1: Texture, T2: Texture> Texture for CheckerboardTexture<T1, T2> {
     fn color(&self, u: f64, v: f64, p: &Vec) -> Color {
         let pitch = std::f64::consts::PI / self.width;
-        let cosines = (pitch * u).cos() * (pitch * v).cos();
+        let sines = (pitch * u).sin() * (pitch * v).sin();
 
-        if cosines < 0. {
+        if sines < 0. {
             self.odd.color(u, v, p)
         } else {
             self.even.color(u, v, p)
