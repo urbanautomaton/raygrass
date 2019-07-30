5e4733621dea98a2f76ad44bd049b3d6592f200e
Simon Coffey <simon@urbanautomaton.com>
2019-07-30 07:58:52 +0100

    Sort out movie scripts
    Separates frame generation from ffmpeg gubbins and sorts out the bash
    quoting.

--- movie.rb
+++
@@ -1,7 +0,0 @@
-(1..250).each do |frame|
-  time = 10.0 * frame / 250
-  `./target/release/raygrass --samples 50 --resolution 1280x960 --time #{time} frames/#{"%03d" % frame}.png`
-end
-
-`ffmpeg -r 25 -pattern_type glob -i 'frames/*.png' -c:v libx264 -pix_fmt yuv420p -strict -2 -acodec aac out.mp4`
-# `ffmpeg -f concat -safe 0 -i <(for i in {1..4}; do printf "file '%s'\n" "${PWD}/out.mp4"; done) -c copy looped.mp4`
